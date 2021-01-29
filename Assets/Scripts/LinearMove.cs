72a1b1d65e15180522522bef1cd5d0350d175fc6
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-29 20:26:14 -0300

    Asteroid & Enemy movement

---
+++ Assets/Scripts/LinearMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LinearMove : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 direction;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    private void Update()
+    {
+        transform.Translate(direction * (moveSpeed * Time.deltaTime));
+    }
+}
